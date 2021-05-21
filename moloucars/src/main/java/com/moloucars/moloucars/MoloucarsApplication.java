57dca8dbb37d8540017048a3a5f1909908c406ab
unknown <alexandre2010nogueira@hotmail.com>
2021-05-21 16:30:20 -0300

    Criação entidades Usuario e Veiculo

---
+++ moloucars/src/main/java/com/moloucars/moloucars/MoloucarsApplication.java
@@ -0,0 +1,13 @@
+package com.moloucars.moloucars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoloucarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoloucarsApplication.class, args);
+	}
+
+}
